50cd685ae82ed1acb087c7b53dd9c27d8c3b0f88
DriftuKing <haremkingg@hotmail.com>
2021-04-08 22:19:03 -0400

    - Increases stats upon spawning.

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -1,47 +1,35 @@
 using BepInEx;
-using R2API;
-using R2API.Utils;
-using RoR2;
-using UnityEngine;
+using On.RoR2;
+using System.Collections.Generic;
 
 namespace ExamplePlugin
 {
-    //This attribute specifies that we have a dependency on R2API, as we're using it to add our item to the game.
-    //You don't need this if you're not using R2API in your plugin, it's just to tell BepInEx to initialize R2API before this plugin so it's safe to use R2API.
-    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.Dual.CustomStats", "CustomStats", "1.0.0")]
 
-    //This attribute is required, and lists metadata for your plugin.
-    [BepInPlugin(
-        //The GUID should be a unique ID for this plugin, which is human readable (as it is used in places like the config). Java package notation is commonly used, which is "com.[your name here].[your plugin name here]"
-        "com.MyName.IHerebyGrantPermissionToDeprecateMyModFromThunderstoreBecauseIHaveNotChangedTheName",
-        //The name is the name of the plugin that's displayed on load
-        "IHerebyGrantPermissionToDeprecateMyModFromThunderstoreBecauseIHaveNotChangedTheName",
-        //The version number just specifies what version the plugin is.
-        "1.0.0")]
-    //Like seriously, if we see this boilerplate on thunderstore, we will deprecate this mod. Change that name!
-    //If you want to test package uploading in general, try using beta.thunderstore.io
-
-    //This is the main declaration of our plugin class. BepInEx searches for all classes inheriting from BaseUnityPlugin to initialize on startup.
-    //BaseUnityPlugin itself inherits from MonoBehaviour, so you can use this as a reference for what you can declare and use in your plugin class: https://docs.unity3d.com/ScriptReference/MonoBehaviour.html
     public class ExamplePlugin : BaseUnityPlugin
     {
+        private bool isBuffApplied = false;
+        private readonly List<string> survivors = new List<string> { "Acrid", "Artificer", "Bandit", "Captain", "Commando", "Engineer", "Huntress", "Loader", "Mercenary", "MUL-T", "REX" };
 
-        //The Awake() method is run at the very start when the game is initialized.
         public void Awake()
         {
-            Logger.LogMessage("Loaded MyModName!");
-            On.EntityStates.Huntress.ArrowRain.OnEnter += (orig, self) =>
-            {
-                // [The code we want to run]
-                Chat.AddMessage("You used Huntress's Arrow Rain!");
-
-                // Call the original function (orig)
-                // on the object it's normally called on (self)
-                orig(self);
-            };
+            GlobalEventManager.OnCharacterHitGround += LevelUpStatsModifier;
         }
 
-        //The Update() method is run on every frame of the game.
+        private void LevelUpStatsModifier(GlobalEventManager.orig_OnCharacterHitGround orig, RoR2.GlobalEventManager self, RoR2.CharacterBody characterBody, UnityEngine.Vector3 impactVelocity)
+        {
+            // Check if the character is a survivor. Without this, the buff is applied to every single entity.
+            // Checking if the buff is applied is not needed right now, but if playing with bonus stats it prevents the stats from increasing non-stop.
+            if (!isBuffApplied && survivors.IndexOf(characterBody.GetDisplayName()) != -1) 
+            {
+                characterBody.baseAttackSpeed = 30f;
+                characterBody.baseCrit = 100f;
+                characterBody.baseDamage = 10f;
+                isBuffApplied = true;
+            }
+            orig(self, characterBody, impactVelocity);
+        }
+
         public void Update()
         {
             
