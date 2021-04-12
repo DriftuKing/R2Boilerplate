92945f81018525949bec99e5b464810e56414147
DriftuKing <haremkingg@hotmail.com>
2021-04-11 21:41:21 -0400

    - messy and not optimized. will do later.

--- ExamplePlugin/CustomStats.cs
+++ ExamplePlugin/CustomStats.cs
@@ -13,20 +13,34 @@ namespace CustomStats
         CharacterStats customCharacter;
         int multiplierVal;
         string user;
+        bool flag;
 
         private void CharacterBody_Start(CharacterBody.orig_Start orig, RoR2.CharacterBody self)
         {
             if (user == self.GetUserName())
             {
                 player = self;
-                InitializeDefaultValues(self);
+                defaultCharacter = new CharacterStats(self.baseAttackSpeed, self.baseDamage, self.baseCrit, self.baseMoveSpeed, self.baseJumpPower, self.baseMaxHealth, self.baseMaxShield, self.baseJumpCount);
+                if (flag)
+                {
+                    customCharacter = new CharacterStats(self.baseAttackSpeed, self.baseDamage, self.baseCrit, self.baseMoveSpeed, self.baseJumpPower, self.baseMaxHealth, self.baseMaxShield, self.baseJumpCount);
+                    flag = false;
+                }
+                InitializeStats(self);
             }
             orig(self);
         }
 
-        public void InitializeDefaultValues(RoR2.CharacterBody self)
+        private void InitializeStats(RoR2.CharacterBody player)
         {
-            defaultCharacter = new CharacterStats(self.baseAttackSpeed, self.baseDamage, self.baseCrit, self.baseMoveSpeed, self.baseJumpPower, self.baseMaxHealth, self.baseMaxShield, self.baseJumpCount);
+            player.baseAttackSpeed = customCharacter.attackSpeed;
+            player.baseDamage = customCharacter.damage;
+            player.baseCrit = customCharacter.crit;
+            player.baseMoveSpeed = customCharacter.moveSpeed;
+            player.baseJumpPower = customCharacter.jumpPower;
+            player.baseJumpCount = customCharacter.jumpCount;
+            player.baseMaxHealth = customCharacter.maxHealth;
+            player.baseMaxShield = customCharacter.maxShield;
         }
 
         private void ResetStats(ref RoR2.CharacterBody player)
@@ -39,26 +53,29 @@ namespace CustomStats
             player.baseJumpCount = defaultCharacter.jumpCount;
             player.baseMaxHealth = defaultCharacter.maxHealth;
             player.baseMaxShield = defaultCharacter.maxShield;
+            customCharacter = new CharacterStats(player.baseAttackSpeed, player.baseDamage, player.baseCrit, player.baseMoveSpeed, player.baseJumpPower, player.baseMaxHealth, player.baseMaxShield, player.baseJumpCount);
             RoR2.Chat.AddMessage(user + "s stats have been <color=#48C9B0>reset</color>.");
         }
 
-        private void IncreaseStat(ref float baseValue, float value, string stat)
+        private void IncreaseStat(ref float baseValue, ref float customBaseValue, float value, string stat)
         {
-            baseValue += value * multiplierVal;
-            if (baseValue < 1f)
+            customBaseValue += value * multiplierVal;
+            if (customBaseValue < 1f)
             {
-                baseValue = 1f;
+                customBaseValue = 1f;
             }
+            baseValue = customBaseValue;
             RoR2.Chat.AddMessage(user + "s " + stat + " changed to <color=#85C1E9>" + baseValue + "</color>.");
         }
 
-        private void IncreaseStat(ref int baseValue, int value, string stat)
+        private void IncreaseStat(ref int baseValue, ref int customBaseValue, int value, string stat)
         {
-            baseValue += value * multiplierVal;
-            if (baseValue < 1)
+            customBaseValue += value * multiplierVal;
+            if (customBaseValue < 1)
             {
-                baseValue = 1;
+                customBaseValue = 1;
             }
+            baseValue = customBaseValue;
             RoR2.Chat.AddMessage(user + "s " + stat + " changed to <color=#85C1E9>" + baseValue + "</color>.");
         }
 
@@ -82,7 +99,13 @@ namespace CustomStats
         {
             CharacterBody.Start += CharacterBody_Start;
             UserProfile.OnLogin += UserProfile_OnLogin;
+            RoR2.Run.onRunStartGlobal += Run_onRunStartGlobal;
             multiplierVal = 1;
+        }
+
+        private void Run_onRunStartGlobal(RoR2.Run obj)
+        {
+            flag = true;
         }
 
         private void UserProfile_OnLogin(UserProfile.orig_OnLogin orig, RoR2.UserProfile self)
@@ -96,35 +119,35 @@ namespace CustomStats
         {
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                IncreaseStat(ref player.baseAttackSpeed, 0.5f, "Attack speed");
+                IncreaseStat(ref player.baseAttackSpeed, ref customCharacter.attackSpeed, 0.5f, "Attack speed");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                IncreaseStat(ref player.baseDamage, 5f, "Damage");
+                IncreaseStat(ref player.baseDamage, ref customCharacter.damage, 5f, "Damage");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                IncreaseStat(ref player.baseCrit, 10f, "Crit chance");
+                IncreaseStat(ref player.baseCrit, ref customCharacter.crit, 10f, "Crit chance");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                IncreaseStat(ref player.baseMoveSpeed, 2f, "Movement speed");
+                IncreaseStat(ref player.baseMoveSpeed, ref customCharacter.moveSpeed, 2f, "Movement speed");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha5))
             {
-                IncreaseStat(ref player.baseJumpPower, 2f, "Jump power");
+                IncreaseStat(ref player.baseJumpPower, ref customCharacter.jumpPower, 2f, "Jump power");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha6))
             {
-                IncreaseStat(ref player.baseJumpCount, 1, "Jump count");
+                IncreaseStat(ref player.baseJumpCount, ref customCharacter.jumpCount, 1, "Jump count");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha7))
             {
-                IncreaseStat(ref player.baseMaxHealth, 50f, "Max health");
+                IncreaseStat(ref player.baseMaxHealth, ref customCharacter.maxHealth, 50f, "Max health");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha8))
             {
-                IncreaseStat(ref player.baseMaxShield, 50f, "Max shield");
+                IncreaseStat(ref player.baseMaxShield, ref customCharacter.maxShield, 50f, "Max shield");
             }
             else if (Input.GetKeyDown(KeyCode.Alpha9))
             {
